a6a9caa42bc2715ed605705792547aa0c2aad803
Yash Patel <49101492+ryuk156@users.noreply.github.com>
2020-10-01 14:00:16 +0530

    added react-presist

--- kanban_board/src/store/index.js
+++ kanban_board/src/store/index.js
@@ -1,7 +1,18 @@
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
+import { createStore } from "redux";
 import rootReducer from "../reducers";
 
-const Store = createStore(rootReducer, applyMiddleware(thunk));
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-export default Store;
+const persistConfig = {
+  key: "root",
+  storage
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export default () => {
+  let store = createStore(persistedReducer);
+  let persistor = persistStore(store);
+  return { store, persistor };
+};
